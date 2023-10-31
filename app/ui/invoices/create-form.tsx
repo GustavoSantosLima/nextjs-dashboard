587af71857884e4b939847e5f02368d38f7ada11
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2023-10-31 14:45:46 -0300

    Finalizando o capítulo 12
    Neste capítulo...
    
    Aqui estão os tópicos que abordaremos
    
    - O que são ações do React Server e como usá-las para alterar dados.
    
    - Como trabalhar com formulários e componentes de servidor.
    
    - Melhores práticas para trabalhar com o formDataobjeto nativo, incluindo validação de tipo.
    
    - Como revalidar o cache do cliente usando a revalidatePathAPI.
    
    - Como criar segmentos de rota dinâmicos com IDs específicos.
    
    - Como usar o useFormStatusgancho do React para atualizações otimistas.

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,18 +1,19 @@
-'use client';
+"use client";
 
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+import { createInvoice } from "@/app/lib/actions";
+import { CustomerField } from "@/app/lib/definitions";
+import { Button } from "@/app/ui/button";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+  UserCircleIcon
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -29,7 +30,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <option value="" disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
+              {customers.map(customer => (
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
