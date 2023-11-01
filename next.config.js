c342142e57a90ec3c9926c087bba80f257048790
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2023-11-01 18:59:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "vercel.com", "vercel.app"]
+    domains: ["localhost", "https://nextjs-dashboard-coral-omega.vercel.app", "vercel.app"]
   }
 };
 
